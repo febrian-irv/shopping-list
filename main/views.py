50cc419a965ed6cb678db3ad35e5a31a0827e865
febrian-irv <febrian.irv@gmail.com>
2023-11-15 13:10:33 +0700

    adjustment for deploy

--- main/views.py
+++ main/views.py
@@ -114,6 +114,7 @@ def get_product_json(request):
     product_item = Product.objects.all()
     return HttpResponse(serializers.serialize('json', product_item))
 
+
 @csrf_exempt
 def add_product_ajax(request):
     if request.method == 'POST':
